220dd6d88b1d96845793285d9f949d52fc762485
Supasan Suklim <supasan.game@gmail.com>
2020-10-03 15:42:27 +0700

    Change mediacard

--- src/commons/MediaCard.js
+++ src/commons/MediaCard.js
@@ -3,65 +3,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import red from '@material-ui/core/colors/red';
+// import { Link } from 'react-router-dom';
 
-const styles = theme => ({
+const styles = {
   card: {
+    maxWidth: '100%',
     marginBottom: 20,
   },
   media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
+    height: 300,
   },
-  actions: {
-    display: 'flex',
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-});
+};
 
 const MediaCard = ({ classes, detail }) => (
   <Card className={classes.card}>
-    <CardHeader
-      avatar={
-        <Avatar aria-label="Recipe" className={classes.avatar}>
-          R
-        </Avatar>
-      }
-      title={detail.author}
-      subheader={detail.url}
-    />
-    {
-      detail.preview ?
-        <CardMedia
-          className={classes.media}
-          image={detail.thumbnail}
-          title={detail.title}
-        /> : null
-    }
-
-    <CardContent>
-      <Typography component="p">
-        {detail.title}
-      </Typography>
-    </CardContent>
-
+    <CardActionArea>
+      <CardMedia
+        className={classes.media}
+        image={detail.thumbnail}
+        title={detail.title}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {detail.subreddit}
+        </Typography>
+        <Typography component="p">
+          {detail.title}
+        </Typography>
+      </CardContent>
+    </CardActionArea>
   </Card>
 );
 
